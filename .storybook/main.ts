ea14816278c459b30ab42ee9af6618cb3985ce9a
aFu <2298773938@qq.com>
2024-03-14 10:47:34 +0800

    build: implement storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,22 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const config: StorybookConfig = {
+  stories: ['../**/*.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+    '@storybook/addon-webpack5-compiler-babel'
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: true,
+  },
+  staticDirs: ['../public'],
+};
+export default config;
